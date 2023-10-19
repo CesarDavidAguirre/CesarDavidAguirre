abfad91adbf4ea8399249bc5886598b47209d08f
Cesar Aguirre <cesaraguirre566@gmail.com>
2023-10-19 19:44:57 +0200

    test

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+function App() {
+  return (
+    <div className="App">
+      <Button>heee</Button>
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default App;
